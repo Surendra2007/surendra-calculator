e08a41f84922eee97d148ec6b97d89067f984175
Surendra Dhaka <saabchoudhary350@gmail.com>
2024-12-05 11:19:26 +0530

    calculator

---
+++ index.js
@@ -0,0 +1,25 @@
+
+let cur="";
+  let input=document.querySelector("input");
+  let ans=input.getAttribute("value");
+  ans=cur;
+  function number(num){
+    cur+=num;
+    ans=cur;
+    input.setAttribute("value",ans)
+  }
+  function cleared(){
+    input.setAttribute("value","")
+  }
+  function operator(str){
+    cur +=str ;
+    input.setAttribute("value",cur)
+  }
+  function deleted(){
+    cur =cur.slice(0,-1) ;
+    input.setAttribute("value",cur)
+  }
+  function Evaluate(){
+    let cal_ans = eval(cur);
+    input.setAttribute("value",cal_ans)
+  }
